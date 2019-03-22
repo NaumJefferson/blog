b4c52ce2b47b81c056a7e542db8f36d1f3434ea3
Naum Jefferson <na1.jefferson@gmail.com>
2019-03-21 22:00:12 -0300

    Rotas

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+
+    /** 
+     * @Route("/")
+    */
+    public function index()
+    {
+        return $this->render('Default\index.html.twig', ['title'=>'Home']);
+    }
+
+}
+
